38634748c26840bbda1c383c7dd71e5850b1758d
Kroatuan <vinicius_lucena@estudante.sc.senai.br>
2021-11-09 18:47:47 -0300

    done

--- pegar.js
+++ pegar.js
@@ -4,4 +4,5 @@ function capturar(){
     console.log("");
     capturando = document.getElementById('valor').value;
     document.getElementById('valor_digitado').innerHTML += capturando + "<br>";
+    document.getElementById('valor').value = " ";
 }
